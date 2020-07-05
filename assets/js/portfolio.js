a65687441f54417b9fc87b9fa9cdf32824ecf7bf
ali-kilinc <alikilinc1986@gmail.com>
2020-07-05 15:08:14 +0300

    add portfolio project details

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -7,20 +7,33 @@ let projectOutbreak = {
 
         '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/TD9HORgvo_w"></iframe></div>',
         '<img src="https://lh3.googleusercontent.com/VoJNXO9rQ0dRy1GSMhmoKNyKSWjtmVqkqpiLE_oYNFXBHGHxnpGD0je8tdZsdXdznJw=w1064-h969-rw" alt="">',
-        '<img src="https://lh3.googleusercontent.com/VoJNXO9rQ0dRy1GSMhmoKNyKSWjtmVqkqpiLE_oYNFXBHGHxnpGD0je8tdZsdXdznJw=w1064-h969-rw" alt="">'
+        '<img src="https://lh3.googleusercontent.com/H4H2v_g1OU85TCB_hrKWOvtkpzOGVk0vT-O2KZrsuYpXUA8xHZ6BQalSDUn9Cotgcjk=w1920-h969-rw" alt="">'
     ]
 };
 
 let projectYelpCamp =
 {
     title: "Yelp Camp",
-    explanation: `A full-stack Node.js web application project from the Udemy course - <a class="a-dark" target="_blank" href="https://www.udemy.com/the-web-developer-bootcamp/" rel="nofollow">The Web Developer Bootcamp by Colt Steele</a>`,
+    explanation: 'A full-stack Node.js web application project from the Udemy course - <a class="a-dark" target="_blank" href="https://www.udemy.com/the-web-developer-bootcamp/" rel="nofollow">The Web Developer Bootcamp by Colt Steele</a>',
     linkText : "View Site",
     link : "#",
     carouselItems : [
-        '<img src="./assets/imgs/yelpCamp1.png" alt="">',
-        '<img src="./assets/imgs/yelpCamp2.png" alt="">',
-        '<img src="./assets/imgs/yelpCamp3.png" alt="">'
+        '<img src="./assets/imgs/yelpCamp1.png">',
+        '<img src="./assets/imgs/yelpCamp2.png">',
+        '<img src="./assets/imgs/yelpCamp3.png">'
+    ]
+};
+
+let projectPortfolio =
+{
+    title: "MK Portfolio",
+    explanation: 'Meryem Kılınç\'s portfolio, a static, single page web site built using HTML, CSS, JS, jQuery and Bootstrap.',
+    linkText : "View Site",
+    link : "http://meryem.kilinc.me",
+    carouselItems : [
+        '<img src="./assets/imgs/mk_portfolio_1.jpg">',
+        '<img src="./assets/imgs/mk_portfolio_2.jpg">',
+        '<img src="./assets/imgs/mk_portfolio_3.jpg">'
     ]
 };
 
@@ -66,6 +79,14 @@ $("#learnMoreYelpCamp").on("click", function()
     $("#overlay").slideDown();
 });
 
+$("#learnMorePortfolio").on("click", function()
+{
+    $("#overlay").load("../../projectDetails.html", function(){
+        populateProjectDetails(projectPortfolio);
+    });
+
+    $("#overlay").slideDown();
+});
 
 $("#overlay").on("click", function(){
     
@@ -86,24 +107,22 @@ function populateProjectDetails(project)
         var carouselItem = "";
         if(i == 0)
         {
-            carouselItem = `<div class="carousel-item active">`;
+            carouselItem = '<div class="carousel-item active">';
         }
         else
         {
-            carouselItem = `<div class="carousel-item">`;
+            carouselItem = '<div class="carousel-item">';
         }
 
-        carouselItem += project.carouselItems[i] + `</div>`;
+        carouselItem += project.carouselItems[i] + '</div>';
         $("#carouselInner").append(carouselItem);
     }
 
-    var info = `<h3>${project.title}</h3>`;
-    info += `<p>${project.explanation}</p>`;
+    var info = '<h3>' + project.title + '</h3>' + '<p>'  + project.explanation + '</p>';
     
     if(project.link != null)
     {
-        info += `<a href="${project.link}" target="_blank" class="btn-portfolio">${project.linkText}</a>`;
+        info += '<a href="' + project.link + '" target="_blank" class="btn-portfolio">' + project.linkText + '</a>';
     }
-    console.log(info);
     $("#projectDetailInfo").append(info);
 }
